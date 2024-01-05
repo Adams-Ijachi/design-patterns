bdd9937ad71a1bfbbedcbeb547ea2b6d9e9da961
Shinra <Adams-Ijachi>
2024-01-05 02:44:59 +0100

    Strategy pattern

--- Strategy/index.php
+++ Strategy/index.php
@@ -3,16 +3,17 @@
 use StrategyPattern\Behaviours\PayWithBitcoin;
 use StrategyPattern\Clients\PaystackPaymentProvider;
 use StrategyPattern\Clients\CryptoPaymentProvider;
+use StrategyPattern\PaymentProvider;
+
 require_once "Clients/PaystackPaymentProvider.php";
 require_once "Clients/CryptoPaymentProvider.php";
 require_once "Behaviours/PayWithBitcoin.php";
+require_once "PaymentProvider.php";
 
 /**
  * This code demonstrates the use of the Strategy design pattern to perform payments using different payment providers.
  */
 
-$paymentProvider = new PaystackPaymentProvider;
-
+$paymentProvider = new PaymentProvider;
 $paymentProvider->setPaymentProvider(new PayWithBitcoin);
-
 $paymentProvider->performPayment();
